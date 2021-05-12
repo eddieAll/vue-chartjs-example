5e0cb8d3ee7d21cb2959d9f85b312c550cfaa10c
Christopher <myerschristopher14@gmail.com>
2021-05-12 10:39:46 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PlanetChart/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PlanetChart from './components/PlanetChart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PlanetChart
   }
 }
 </script>
